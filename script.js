867f15edda1a6340f3a9df97ca0457abbaf723ed
yuki <s114268@ms2.kghs.kh.edu.tw>
2026-01-08 01:33:28 +0800

    Implement switchScene function for scene changes
    Add switchScene function to change background and audio

--- script.js
+++ script.js
@@ -19,3 +19,13 @@ function switchScene(type) {
     console.log("Audio blocked until user interaction");
   });
 }
+function switchScene(type) {
+  scene.style.backgroundImage = `url(${scenes[type].image})`;
+
+  audio.pause();                // ① 先停
+  audio.src = scenes[type].sound;
+  audio.load();                 // ② ⭐ 關鍵：強制重新載入
+  audio.play().catch(() => {
+    console.log("Audio blocked until user interaction");
+  });
+}
