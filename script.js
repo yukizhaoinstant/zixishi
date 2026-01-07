5b0008adf74706a7737a4569938b8ada87750d2a
yuki <s114268@ms2.kghs.kh.edu.tw>
2026-01-08 00:47:27 +0800

    Change day scene URL to local image

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function changeScene(type) {
   const scene = document.getElementById("scene");
 
   const scenes = {
-    day: "https://github.com/yukizhaoinstant/zixishi/commit/75f7c7c2e022a8c22dcb09c04af61728277a6529#diff-ddc231cd169c9679b4f7c9b1ffc0fa2a814d57fe1d6f22853674286172d5fea1",
+    day: "IMG_1333.jpeg",
     night: "assets/images/room_night.jpg",
     rain: "assets/images/rain_window.jpg"
   };
